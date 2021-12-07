1dd3ad26423545acd31730c75197883fe0b0e6e7
shamelfahmi <a.shamel.fahmi@gmail.com>
2021-12-07 14:01:43 -0500

    gravity is ON by default ("disable_gravity" is set to false because it was messing up the training of anymal_c)

--- gpugym/envs/base/legged_robot_config.py
+++ gpugym/envs/base/legged_robot_config.py
@@ -119,8 +119,11 @@ class LeggedRobotCfg(BaseConfig):
         penalize_contacts_on = []
         terminate_after_contacts_on = []
         disable_gravity = False
+<<<<<<< HEAD
         disable_actions = False
         disable_motors = False
+=======
+>>>>>>> gravity is ON by default ("disable_gravity" is set to false because it was messing up the training of anymal_c)
         collapse_fixed_joints = True # merge bodies connected by fixed joints. Specific fixed joints can be kept by adding " <... dont_collapse="true">
         fix_base_link = False # fixe the base of the robot
         default_dof_drive_mode = 3 # see GymDofDriveModeFlags (0 is none, 1 is pos tgt, 2 is vel tgt, 3 effort)
