18ef63a99a30cfb2335803df4befff7e7e87bff5
Steve Heim <heim.steve@gmail.com>
2022-09-28 09:06:11 -0400

    update fixed_robot_config with

--- gpugym/envs/base/fixed_robot_config.py
+++ gpugym/envs/base/fixed_robot_config.py
@@ -181,6 +181,7 @@ class FixedRobotCfgPPO(BaseConfig):
         algorithm_class_name = 'PPO'
         num_steps_per_env = 24 # per iteration
         max_iterations = 1500 # number of policy updates
+        SE_learner = None
 
         # logging
         save_interval = 50 # check for potential saves every this many iterations
