fix avg_action bug, and change rewards to correct sign (so in the config file, all weights should be non-negative)

gpugym/envs/base/legged_robot.py
@@ -100,7 +100,7 @@ class LeggedRobot(BaseTask):
         # step physics and render each frame
         self.render()
         for _ in range(self.cfg.control.decimation):
-            self.torques = self._compute_torques(self.action_avg).view(self.torques.shape)
+            self.torques = self._compute_torques(actions).view(self.torques.shape)
 
             if self.cfg.asset.disable_motors:
                 self.torques[:] = 0.
@@ -949,7 +949,7 @@ class LeggedRobot(BaseTask):
 
     def _reward_torques(self):
         # Penalize torques
-        return torch.sum(torch.square(self.torques), dim=1)
+        return -torch.sum(torch.square(self.torques), dim=1)
 
 
     def _reward_dof_vel(self):
@@ -963,7 +963,7 @@ class LeggedRobot(BaseTask):
         dt2 = (self.dt*self.cfg.control.decimation)**2
         error = torch.square(self.ctrl_hist[:, :nact] \
                             - self.ctrl_hist[:, nact:2*nact])/dt2
-        return torch.sum(error, dim=1)
+        return -torch.sum(error, dim=1)
 
 
     def _reward_action_rate2(self):
@@ -973,12 +973,12 @@ class LeggedRobot(BaseTask):
         error = torch.square(self.ctrl_hist[:, :nact]  \
                             - 2*self.ctrl_hist[:, nact:2*nact]  \
                             + self.ctrl_hist[:, 2*nact:])/dt2
-        return torch.sum(error, dim=1)
+        return -torch.sum(error, dim=1)
 
 
     def _reward_collision(self):
         # Penalize collisions on selected bodies
-        return torch.sum(1.*(torch.norm(self.contact_forces[:, self.penalised_contact_indices, :], dim=-1) > 0.1), dim=1)
+        return -torch.sum(1.*(torch.norm(self.contact_forces[:, self.penalised_contact_indices, :], dim=-1) > 0.1), dim=1)
 
 
     def _reward_termination(self):

gpugym/envs/mini_cheetah/mini_cheetah_ref.py
@@ -64,7 +64,7 @@ class MiniCheetahRef(MiniCheetah):
         nact = self.num_actions
         self.ctrl_hist[:, 2*nact:] = self.ctrl_hist[:, nact:2*nact]
         self.ctrl_hist[:, nact:2*nact] = self.ctrl_hist[:, :nact]
-        self.ctrl_hist[:, :nact] = self.action_avg
+        self.ctrl_hist[:, :nact] = self.actions
 
         # ? unsqueeze
         self.phase_obs = torch.cat([torch.cos(self.phase),
@@ -120,10 +120,10 @@ class MiniCheetahRef(MiniCheetah):
 
         if self.cfg.control.exp_avg_decay:
             self.action_avg = exp_avg_filter(self.actions, self.action_avg,
-                                            self.cfg.control.exp_avg_decay)
+                                             self.cfg.control.exp_avg_decay)
             actions = self.action_avg
 
-        if self.cfg.control.control_type=="P":
+        if self.cfg.control.control_type == "P":
 
             torques = self.p_gains*(actions * self.cfg.control.action_scale \
                                     + self.default_dof_pos \
@@ -199,19 +199,19 @@ class MiniCheetahRef(MiniCheetah):
 
     def _reward_swing_grf(self):
         # Reward non-zero grf during swing (0 to pi)
-        grf = torch.gt(torch.norm(self.contact_forces[:, self.feet_indices, :], dim=-1), 50.)
-        ph_off = torch.lt(self.phase, torch.pi)  # should this be in swing?
-        rew = grf*torch.cat((ph_off, ~ph_off, ~ph_off, ph_off), dim=1).int()
-        return torch.sum(rew, dim=1)*(1-self.switch())
+        in_contact = torch.gt(torch.norm(self.contact_forces[:, self.feet_indices, :], dim=-1), 50.)
+        ph_off = torch.lt(self.phase, torch.pi)
+        rew = in_contact*torch.cat((ph_off, ~ph_off, ~ph_off, ph_off), dim=1)
+        return -torch.sum(rew.float(), dim=1)*(1-self.switch())
 
 
     def _reward_stance_grf(self):
         # Reward non-zero grf during stance (pi to 2pi)
-        grf = torch.gt(torch.norm(self.contact_forces[:, self.feet_indices, :], dim=-1), 50.)
+        in_contact = torch.gt(torch.norm(self.contact_forces[:, self.feet_indices, :], dim=-1), 50.)
         ph_off = torch.gt(self.phase, torch.pi)  # should this be in swing?
-        rew = grf*torch.cat((ph_off, ~ph_off, ~ph_off, ph_off), dim=1).int()
+        rew = in_contact*torch.cat((ph_off, ~ph_off, ~ph_off, ph_off), dim=1)
 
-        return torch.sum(rew, dim=1)*(1-self.switch())
+        return torch.sum(rew.float(), dim=1)*(1-self.switch())
 
 
     def _reward_reference_traj(self):