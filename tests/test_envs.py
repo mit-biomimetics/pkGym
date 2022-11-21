98d2c822f35769b6b461e5b6c4b49483d0b10bd3
hojae <hjlee201@mit.edu>
2022-11-21 15:50:47 -0500

    test envs' one step()

---
+++ tests/test_envs.py
@@ -0,0 +1,33 @@
+import unittest
+from gym.envs import *
+from gym.utils import get_args, task_registry
+import torch
+import isaacgym
+from gym.utils import class_to_dict
+
+class TestEnvs(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.args = get_args()
+        self.args.headless = True
+        task_registry.make_gym()
+        self.env_names = list(task_registry.task_classes.keys())
+
+    def test_one_step(self):
+        for env_name in self.env_names:
+            self.args.task = env_name
+            env_cfg, train_cfg = task_registry.create_cfgs(self.args)
+            task_registry.update_sim_cfg(self.args)
+            task_registry.make_sim()
+            env, _ = task_registry.make_env(name=env_name,
+                                            env_cfg=env_cfg)
+
+            env.step(torch.zeros(env.num_envs, env.num_actions, device=env.device, requires_grad=False))
+            self.assertEqual(len(env.extras), 0, "env.extras has been modified!")
+            
+            task_registry._gym.destroy_sim(task_registry._sim)
+
+
+if __name__ == '__main__':
+    unittest.main()
