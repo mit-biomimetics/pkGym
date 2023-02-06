beda78dea77102e836dba4e9fdd4827f85840a30
AJ Miller <ajm4@mit.edu>
2023-02-06 17:55:48 -0500

    Linted sim_config.py

--- gym/envs/base/sim_config.py
+++ gym/envs/base/sim_config.py
@@ -1,7 +1,7 @@
 class SimCfg:
     dt = 0.001  # calculated based on env.config.control.desired_sim_frequency
     substeps = 1
-    gravity = [0., 0. , -9.81]  # [m/s^2]
+    gravity = [0., 0., -9.81]  # [m/s^2]
     up_axis = 1  # 0 is y, 1 is z
 
     class physx:
@@ -11,8 +11,10 @@ class SimCfg:
         num_velocity_iterations = 0
         contact_offset = 0.01
         rest_offset = 0.0
-        bounce_threshold_velocity = 0.5 #0.5 [m/s]
-        contact_collection = 2 # 0: never, 1: last sub-step, 2: all sub-steps (default=2)
+        bounce_threshold_velocity = 0.5  # 0.5 [m/s]
+        # * 0: never, 1: last sub-step, 2: all sub-steps (default=2)
+        contact_collection = 2
         max_depenetration_velocity = 10.0
-        max_gpu_contact_pairs = 2**23  #2**24 needed for 8000 envs or more
+        # * 2**24 needed for 8000 envs or more
+        max_gpu_contact_pairs = 2**23
         default_buffer_size_multiplier = 5
