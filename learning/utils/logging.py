8f2dfdb1e994b46cf10eb4cbbcd43bb64ff55816
AJ Miller <ajm4@mit.edu>
2023-02-08 14:56:21 -0500

    Linted learning/utils

--- learning/utils/logging.py
+++ learning/utils/logging.py
@@ -1,10 +1,10 @@
+import os
 import wandb
 import torch
+import shutil
+import fnmatch
 from collections import deque
 from statistics import mean
-import os
-import fnmatch
-import shutil
 
 
 class Logger:
@@ -21,14 +21,17 @@ class Logger:
         self.max_episode_length_s = max_episode_length_s
 
     def initialize_buffers(self, num_envs, reward_keys):
-        self.current_episode_return = \
-            {name: torch.zeros(num_envs, dtype=torch.float, device=self.device,
-                               requires_grad=False) for name in reward_keys}
-        self.current_episode_length = torch.zeros(num_envs,
-                                                  dtype=torch.float,
-                                                  device=self.device)
-        self.avg_return_buffer = {name:  deque(maxlen=self.avg_window)
-                                  for name in reward_keys}
+        self.current_episode_return = {
+            name: torch.zeros(
+                num_envs, dtype=torch.float,
+                device=self.device, requires_grad=False)
+            for name in reward_keys}
+        self.current_episode_length = torch.zeros(
+            num_envs,
+            dtype=torch.float, device=self.device)
+        self.avg_return_buffer = {
+            name:  deque(maxlen=self.avg_window)
+            for name in reward_keys}
         self.avg_length_buffer = deque(maxlen=self.avg_window)
         self.mean_rewards = {"Episode/"+name:  0. for name in reward_keys}
 
@@ -45,52 +48,65 @@ class Logger:
 
     def log_current_reward(self, name, reward):
         if name in self.current_episode_return.keys():
-            self.current_episode_return[name] += reward  
+            self.current_episode_return[name] += reward
 
     def update_episode_buffer(self, dones):
         self.current_episode_length += 1
         terminated_ids = torch.where(dones == True)[0]
         for name in self.current_episode_return.keys():
             self.avg_return_buffer[name].extend(
-                                    self.current_episode_return[name]
-                                    [terminated_ids].cpu().numpy().tolist())
+                self.current_episode_return[name]
+                [terminated_ids].cpu().numpy().tolist())
             self.current_episode_return[name][terminated_ids] = 0.
 
-        self.avg_length_buffer.extend(self.current_episode_length[terminated_ids]
-                                        .cpu().numpy().tolist())
+        self.avg_length_buffer.extend(
+            self.current_episode_length[terminated_ids].cpu().numpy().tolist())
         self.current_episode_length[terminated_ids] = 0
         if (len(self.avg_length_buffer) > 0):
             self.calculate_reward_avg()
 
     def calculate_reward_avg(self):
         self.mean_episode_length = mean(self.avg_length_buffer)
-        self.mean_rewards = {"Episode/"+name:  mean(self.avg_return_buffer[name]) / self.max_episode_length_s
-                        for name in  self.current_episode_return.keys()} 
+        self.mean_rewards = {
+            "Episode/"+name:
+                mean(self.avg_return_buffer[name]) / self.max_episode_length_s
+            for name in self.current_episode_return.keys()}
         self.total_mean_reward = sum(list(self.mean_rewards.values()))
 
     def print_to_terminal(self):
-        width=80
-        pad=35
+        width = 80
+        pad = 35
         str = f" \033[1m Learning iteration {self.it}/{self.tot_iter} \033[0m "
 
-        log_string = (f"""{'#' * width}\n"""
+        log_string = (
+            f"""{'#' * width}\n"""
             f"""{str.center(width, ' ')}\n\n"""
-            f"""{'Computation:':>{pad}} {self.log['Perf/total_fps']:.0f} steps/s (collection: {self.log[
-            'Perf/collection_time']:.3f}s, learning {self.log['Perf/learning_time']:.3f}s)\n"""
-            f"""{'Value function loss:':>{pad}} {self.log['Loss/value_function']:.4f}\n"""
-            f"""{'Surrogate loss:':>{pad}} {self.log['Loss/surrogate']:.4f}\n"""
-            f"""{'Mean action noise std:':>{pad}} {self.log['Policy/mean_noise_std']:.2f}\n"""
-            f"""{'Mean episode length:':>{pad}} {self.log['Train/mean_episode_length']:.2f}\n"""
+            f"""{'Computation:':>{pad}} {
+                self.log['Perf/total_fps']:.0f} steps/s (collection: {
+                self.log['Perf/collection_time']:.3f}s, learning {
+                self.log['Perf/learning_time']:.3f}s)\n"""
+            f"""{'Value function loss:':>{pad}} {
+                self.log['Loss/value_function']:.4f}\n"""
+            f"""{'Surrogate loss:':>{pad}} {
+                self.log['Loss/surrogate']:.4f}\n"""
+            f"""{'Mean action noise std:':>{pad}} {
+                self.log['Policy/mean_noise_std']:.2f}\n"""
+            f"""{'Mean episode length:':>{pad}} {
+                self.log['Train/mean_episode_length']:.2f}\n"""
             f"""{'Mean reward:':>{pad}} {self.total_mean_reward:.2f}\n""")
         log_string += (f"""{'-' * width}\n""")
         for key, value in self.mean_rewards.items():
-            log_string += f"""{f'Mean episode {key[8:]}:':>{pad}} {value:.4f}\n"""
-        log_string += (f"""{'-' * width}\n"""
-                       f"""{'Total timesteps:':>{pad}} {self.log['Train/total_timesteps']}\n"""
-                       f"""{'Iteration time:':>{pad}} {self.log['Train/iteration_time']:.2f}s\n"""
-                       f"""{'Total time:':>{pad}} {self.log['Train/time']:.2f}s\n"""
-                       f"""{'ETA:':>{pad}} {self.log['Train/time'] / (self.it + 1) * (
-                               self.learning_iter - self.it):.1f}s\n""")
+            log_string += f"""{
+                f'Mean episode {key[8:]}:':>{pad}} {value:.4f}\n"""
+        log_string += (
+            f"""{'-' * width}\n"""
+            f"""{'Total timesteps:':>{pad}} {
+                self.log['Train/total_timesteps']}\n"""
+            f"""{'Iteration time:':>{pad}} {
+                self.log['Train/iteration_time']:.2f}s\n"""
+            f"""{'Total time:':>{pad}} {self.log['Train/time']:.2f}s\n"""
+            f"""{'ETA:':>{pad}} {self.log['Train/time'] / (self.it + 1) * (
+                self.learning_iter - self.it):.1f}s\n""")
 
         print(log_string)
 
