b0fb2003550779ba1584b7d3cdb79cdd9c00829b
Steve <heim.steve@gmail.com>
2023-03-06 18:26:46 -0500

    128 cartpole rewards not calculating correctly (#135)
    * working training pipeline for cartpole to actually learn swing-up
    
    * clean up play
    
    * make cartpole physically slower (longer pole), some tuning. swings up, doesn't balance perfectly
    
    * Tweak reward function and weights for more consistent balancing. Trains in approx 500-750 iterations
    
    * working training pipeline for cartpole to actually learn swing-up
    
    * clean up play
    
    * make cartpole physically slower (longer pole), some tuning. swings up, doesn't balance perfectly
    
    * Tweak reward function and weights for more consistent balancing. Trains in approx 500-750 iterations
    
    * tweaked scalings, added a reward for energy pumping
    
    ---------
    
    Co-authored-by: kendrick <ke26966@mit.edu>

--- gym/envs/cartpole/cartpole.py
+++ gym/envs/cartpole/cartpole.py
@@ -62,6 +62,9 @@ class Cartpole(FixedRobot):
         self.cart_vel_square = self.dof_vel[:, 0:1].square()
         self.pole_vel_square = self.dof_vel[:, 1:2].square()
 
+    def _compute_torques(self):
+        return self.tau_ff
+
     def _reward_pole_pos(self):
         return torch.cos(self.dof_pos[:, 1])
 
@@ -74,4 +77,22 @@ class Cartpole(FixedRobot):
         return -cart_pos.square()
 
     def _reward_upright_pole(self):
-        return self._sqrdexp(1.-torch.cos(self.dof_pos[:, 1]))
+        return torch.cos(self.dof_pos[:, 1])
+
+    def _reward_energy(self):
+        m_cart = 1.
+        m_pole = 1.
+        l_pole = 2.
+        kinetic_energy = (0.5*(m_cart + m_pole)*self.dof_vel[:, 0].square()
+                          + 0.5*m_pole * l_pole**2 * self.dof_vel[:, 1].square()
+                          + self.dof_vel[:, 0]*self.dof_vel[:, 1]*l_pole**2/2.
+                          * torch.sin(self.dof_pos[:, 1]))
+        potential_energy = m_pole*9.81*l_pole*torch.cos(self.dof_pos[:, 1])
+        upright_energy = m_pole*9.81*l_pole
+
+        energy_error = kinetic_energy + potential_energy - upright_energy
+        reward = self._sqrdexp(energy_error/upright_energy) \
+                 + self._sqrdexp(energy_error/upright_energy*10.)
+
+        return reward
+
