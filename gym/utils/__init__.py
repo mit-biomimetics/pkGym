c491eb262657abab1a73d1ccbdc799c847f45f71
Steve <heim.steve@gmail.com>
2023-03-26 21:05:08 +0200

    Test utils (#158)
    * add tests for  and
    
    * zap deprecated logger file
    
    * linting
    
    * add assert testing
    
    * Added positive dt assertion to set_discount
    
    * Now tests assertionerrors separately
    
    * move assertion on positive time-step to always be evaluated
    
    ---------
    
    Co-authored-by: Jenny Zhang <jenny.l.zhang@gmail.com>

--- gym/utils/__init__.py
+++ gym/utils/__init__.py
@@ -32,7 +32,6 @@ from .helpers import (class_to_dict, get_load_path, get_args, set_seed,
                       update_class_from_dict)
 
 from .task_registry import task_registry
-from .logger import Logger
 from .math import *
 from .terrain import Terrain
 from .interfaces.interfaces import KeyboardInterface, GamepadInterface
