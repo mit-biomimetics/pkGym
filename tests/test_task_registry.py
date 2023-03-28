40c0f2bfc47b37c5b9236025cffe036c3893ac60
ajm4mit <57418683+ajm4mit@users.noreply.github.com>
2023-03-28 14:04:23 -0400

    Fixed argv mocking for unit tests (#160)
    * Reworked argv to be mocked to fix gpuGym args bug
    
    - mocked sessionstart's argv with a patch.object
    - mocked the args fixture
    - fixed some small lints
    
    * Remove old comment

--- tests/test_task_registry.py
+++ tests/test_task_registry.py
@@ -1,10 +1,8 @@
-from gym.utils.logging_and_saving import local_code_save_helper
 from gym.utils.logging_and_saving import wandb_singleton
-from gym.utils import get_args
+
 
 class TestTaskRegistry:
     def test_naming_sync(self, task_registry, env_cfg, train_cfg, args):
-        args = get_args()
         env_cfg, train_cfg = task_registry.create_cfgs(args)
         wandb_helper = wandb_singleton.WandbSingleton()
         task_registry.set_log_dir_name(train_cfg)
