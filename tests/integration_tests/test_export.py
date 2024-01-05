ab2018c8974a43553604a09c9e482b684010126c
Steve Heim <heim.steve@gmail.com>
2024-01-05 01:40:02 -0500

    cleaned up, tightened tolerances, test for differences between original and loaded as well

--- tests/integration_tests/test_export.py
+++ tests/integration_tests/test_export.py
@@ -28,34 +28,43 @@ def export_policy():
     return None
 
 
+def load_saved_policy(runner):
+    num_actor_obs = runner.get_obs_size(runner.policy_cfg["actor_obs"])
+    num_critic_obs = runner.get_obs_size(runner.policy_cfg["critic_obs"])
+    num_actions = runner.get_action_size(runner.policy_cfg["actions"])
+    actor_critic = ActorCritic(
+        num_actor_obs, num_critic_obs, num_actions, **runner.policy_cfg
+    ).to(runner.device)
+    resume_path = get_load_path(
+        name=runner.cfg["experiment_name"],
+        load_run=runner.cfg["load_run"],
+        checkpoint=runner.cfg["checkpoint"],
+    )
+    loaded_dict = torch.load(resume_path)
+    actor_critic.load_state_dict(loaded_dict["model_state_dict"])
+    actor_critic.eval()
+    return actor_critic
+
+
 class TestONNX:
     def test_onnx_vs_pt(self, args):
-        args.task = "mini_cheetah"
-        args.max_iterations = 10
-        args.save_interval = 10
+        # set up a runner and policy
+        args.task = "humanoid"
+        args.max_iterations = 1
+        args.save_interval = 1
         args.headless = True
         args.disable_wandb = True
 
         runner = learn_policy(args)
         runner.switch_to_eval()
+
         # load the saved policy
-        num_actor_obs = runner.get_obs_size(runner.policy_cfg["actor_obs"])
-        num_critic_obs = runner.get_obs_size(runner.policy_cfg["critic_obs"])
-        num_actions = runner.get_action_size(runner.policy_cfg["actions"])
-        actor_critic = ActorCritic(
-            num_actor_obs, num_critic_obs, num_actions, **runner.policy_cfg
-        ).to(runner.device)
-        resume_path = get_load_path(
-            name=runner.cfg["experiment_name"],
-            load_run=runner.cfg["load_run"],
-            checkpoint=runner.cfg["checkpoint"],
-        )
-        loaded_dict = torch.load(resume_path)
-        actor_critic.load_state_dict(loaded_dict["model_state_dict"])
+        loaded_actor_critic = load_saved_policy(runner)
 
         # eport the saved policy
         export_path = "exported_policy"
-        actor_critic.export_policy(export_path)
+        loaded_actor_critic.export_policy(export_path)
+
         # load the exported onnx
         path_to_onnx = os.path.join(export_path, "policy.onnx")
         onnx_model = onnx.load(path_to_onnx)
@@ -66,24 +75,20 @@ class TestONNX:
             path_to_onnx, providers=["CPUExecutionProvider"]
         )
 
-        def to_numpy(tensor):
-            return (
-                tensor.detach().cpu().numpy()
-                if tensor.requires_grad
-                else tensor.cpu().numpy()
-            )
-
         # compute torch output
         with torch.no_grad():
             test_input = runner.get_obs(runner.policy_cfg["actor_obs"])[0:1]
-            torch_out = runner.alg.actor_critic.actor.act_inference(test_input)
+            runner_out = runner.alg.actor_critic.actor.act_inference(test_input)
+            loaded_out = loaded_actor_critic.actor.act_inference(test_input)
+
         # compute ONNX Runtime output prediction
-        ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(test_input)}
+        ort_inputs = {ort_session.get_inputs()[0].name: test_input.cpu().numpy()}
         ort_out = ort_session.run(None, ort_inputs)
 
         # compare ONNX Runtime and PyTorch results
-        print(torch_out)
-        print(ort_out)
-        torch.testing.assert_allclose(
-            to_numpy(torch_out), ort_out[0], rtol=1e-03, atol=1e-05
+        torch.testing.assert_close(
+            runner_out.cpu().numpy(), loaded_out.cpu().numpy(), rtol=1e-05, atol=1e-8
+        )
+        torch.testing.assert_close(
+            runner_out.cpu().numpy(), ort_out[0], rtol=1e-05, atol=1e-8
         )
